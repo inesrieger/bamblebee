476b81d99303c0c946086b3f6cf1555ad591d454
Anne Schleicher <anne-maria.schleicher@stud.uni-bamberg.de>
2017-02-23 20:41:16 +0100

    Wroter writer

--- GoogleHashCodeQualificationRound/src/Main.java
+++ GoogleHashCodeQualificationRound/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		Reader ourReader = new Reader("resources/me_at_the_zoo.in");
 		ourReader.readFile();
+		// Writer ourWriter = new Writer();
+		// ourWriter.writeOutput();
 	}
 }
