b6e4fe5f34888ddec03f2ccdc918ced930ab31c0
Anne Schleicher <anne-maria.schleicher@stud.uni-bamberg.de>
2017-02-23 19:56:08 +0100

    sdfs

--- GoogleHashCodeQualificationRound/src/Reader.java
+++ GoogleHashCodeQualificationRound/src/Reader.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Reader {
 	String fileName;
@@ -9,12 +10,15 @@ public class Reader {
 	int numberRequestDescriptions = 0;
 	int numberOfCaches = 0;
 	int sizeOfEachCache = 0;
+	ArrayList<Video> allVideos = new ArrayList<>();
+	ArrayList<Endpoint> allEndpoints = new ArrayList<>();
+	ArrayList<Request> allRequests = new ArrayList<>();
 
 	public Reader(String fileName) {
 		this.fileName = fileName;
 	}
 
-	public int readFile() throws Exception {
+	public void readFile() throws Exception {
 		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
 
 			String line = reader.readLine();
@@ -27,13 +31,20 @@ public class Reader {
 
 			// 2. line
 			line = reader.readLine();
+			while (line.equals("")) {
+				line = reader.readLine();
+			}
+
 			tokens = line.split(" ");
 			int[] sizesOfVideos = new int[numberOfVideos];
 			for (int i = 0; i < tokens.length; i++) {
 				sizesOfVideos[i] = Integer.parseInt(tokens[i]);
+				Video video = new Video(sizesOfVideos[i], i);
+				allVideos.add(video);
 			}
 			int idOfEndpoint = 0;
 			for (int i = 0; i < numberOfEndpoints; i++) {
+
 				// 3. line
 				line = reader.readLine();
 				tokens = line.split(" ");
@@ -44,17 +55,25 @@ public class Reader {
 
 				// 4. line and onwards
 				for (int i1 = 0; i1 < numberOfCaches; i1++) {
+
 					tokens = line.split(" ");
+					System.out.println(Integer.parseInt(tokens[0]));
+
 					CacheServer cacheServer = new CacheServer(Integer.parseInt(tokens[0]), sizeOfEachCache);
+					endpoint.cacheServers[i1] = cacheServer;
 					endpoint.latencyPerCacheServer[i1] = Integer.parseInt(tokens[1]);
 				}
+				allEndpoints.add(endpoint);
 			}
 
 			// Lines for Requests
 			while (line != null) {
 				line = reader.readLine();
 				tokens = line.split(" ");
-				Video video = new Video(Integer.parseInt(tokens[2]), Integer.parseInt(tokens[0]));
+				int videoId = Integer.parseInt(tokens[0]);
+				int endpointId = Integer.parseInt(tokens[1]);
+				Request request = new Request(allVideos.get(videoId), allEndpoints.get(endpointId),
+						Integer.parseInt(tokens[2]));
 				// videoNumber = tokens[0];
 				// fromEndpoint = tokens[1];
 				// numberOfRequests = tokens[2];
