a49888d37591a0b244bc417dd1856f167ff903d8
Anne Schleicher <anne-maria.schleicher@stud.uni-bamberg.de>
2017-02-23 21:08:29 +0100

    Erweitert Methode insertVideo

--- GoogleHashCodeQualificationRound/src/CacheServer.java
+++ GoogleHashCodeQualificationRound/src/CacheServer.java
@@ -1,18 +1,24 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class CacheServer {
 	int capacity = 0;
 	int id = 0;
-	
+
 	ArrayList<Video> videos = new ArrayList<Video>();
-	
-	public CacheServer(int id, int capacity){
+
+	public CacheServer(int id, int capacity) {
 		this.id = id;
 		this.capacity = capacity;
 	}
-	
-	public void insertVideo (Video video){
-		videos.add(video);
+
+	public void insertVideo(Video video) {
+		if (capacity >= video.size) {
+			videos.add(video);
+			capacity = capacity - video.size;
+
+		} else {
+			throw new IllegalArgumentException(
+					"There is not enough place in the CacheServer. CacheServer, method: insertVideo");
+		}
 	}
 }
